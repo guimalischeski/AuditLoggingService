00a87b6d6aa60cee4bdb20eab1d63223a2674e27
guimalischeski <guilhermemalischeski@gmail.com>
2026-02-10 11:39:15 -0300

    Removing unecessary Constants messages;

--- ALS.Core/Constants/Constants.cs
+++ ALS.Core/Constants/Constants.cs
@@ -27,9 +27,6 @@
             public const string GetErrorWithMessage = "Error while querying database. Exception message: {message}.";
             public const string PostError = "Error while ingesting audit instance.";
             public const string PostErrorWithMessage = "Error while ingesting audit instance. Exception message: {message}.";
-
-            //Worker
-            public const string WorkerDelayMillisecondsNotConfigured = "Worker:DelayMilliseconds is not configured.";
         }
 
         public static class LogMessages
@@ -41,9 +38,6 @@
             //SQS
             public const string SqsReachable = "SQS reachable.";
             public const string SqsUnreachable = "SQS unreachable.";
-
-            //Worker
-            public const string WorkerRunningAt = "Worker running at: {time}";
         }
 
         public static class ConfigurationKeys
@@ -56,26 +50,12 @@
             public const string ServiceUrl = "ServiceUrl";
             public const string Region = "Region";
             public const string QueueName = "QueueName";
-
-            //Worker
-            public const string Worker = "Worker";
-            public const string DelayMilliseconds = "DelayMilliseconds";
         }
 
         public static class SortOptions
         {
             public const string TimestampAsc = "timestamp";
             public const string TimestampDesc = "timestamp_desc";
-        }
-
-        public static class AuditMetrics
-        {
-            public const string IngestedTotalKey = "als_events_ingested_total";
-            public const string IngestedTotalDescription = "Total ingested events count.";
-            public const string ConsumerErrorTotalKey = "als_queue_consumer_errors_total";
-            public const string ConsumerErrorTotalDescription = "Consumer error count.";
-            public const string ProcessingTimeInSecondsKey = "als_processing_time_seconds";
-            public const string ProcessingTimeInSecondsDescription = "Processing time in seconds.";
         }
     }
 }
